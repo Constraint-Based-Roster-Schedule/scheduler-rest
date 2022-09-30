e963f17c8444a51687daa5dbcec62f9a6a40540d
Gamunu Bandara <gamunu99sb@gmail.com>
2022-09-30 12:59:02 +0530

    add exchange request model

---
+++ models/exchangeRequest.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const exchangeRequestSchema = new mongoose.Schema({
+  fromID: {
+    type: String,
+    required: true,
+  },
+  toID: {
+    type: String,
+    required: true
+  },
+  currentDate: {
+    type: Number, 
+    required: true
+  },
+  requestedDate: {
+    type: Number, 
+    required: true
+  },
+  requestState: {
+    type: Number,
+    required: true
+  }
+});
+
+const exchangeRequest = mongoose.model("exchangeRequest", exchangeRequestSchema);
+
+module.exports = User;
